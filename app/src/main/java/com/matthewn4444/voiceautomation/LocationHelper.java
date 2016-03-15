cb1a80553d665df72c6a97ba3cfc717e4723e20a
Matthew Ng <matthewn4444@gmail.com>
2016-03-14 22:58:04 -0700

    Removed some libraries and changed the locationhelper class

--- app/src/main/java/com/matthewn4444/voiceautomation/LocationHelper.java
+++ app/src/main/java/com/matthewn4444/voiceautomation/LocationHelper.java
@@ -7,30 +7,29 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
+import android.util.Log;
 
-import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
-import java.util.concurrent.TimeUnit;
-
-import pl.charmas.android.reactivelocation.ReactiveLocationProvider;
-import rx.Subscription;
-import rx.functions.Action1;
-
-public class LocationHelper {
+public class LocationHelper implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+    private static final String TAG = "LocationHelper";
     private static final String HighAccuracyProvider = LocationManager.GPS_PROVIDER;
     private static final String LowAccuracyProvider = LocationManager.NETWORK_PROVIDER;
-    private static final int SubscriptionTimeout = 10;      // 10 Sec
 
     private final Context mCtx;
     private final String mProvider;
     private final LocationManager mManager;
 
-    private Subscription mSubscription;
     private Location mLocation;
     private boolean mAskToEnableLocation;
     private OnLocationFoundListener mFoundListener;
+    private GoogleApiClient mApiClient;
 
     public interface OnLocationFoundListener {
         void onLocationFound(Location location);
@@ -45,6 +44,12 @@ public class LocationHelper {
         mProvider = useHighAccuracy ? HighAccuracyProvider : LowAccuracyProvider;
         mManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         mAskToEnableLocation = false;
+
+        mApiClient = new GoogleApiClient.Builder(context)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     public static boolean isLocationHardcoded(Context context) {
@@ -67,9 +72,13 @@ public class LocationHelper {
             return;
         }
 
-        mFoundListener = listener;
         if (hasLocationEnabled()) {
-            internalQueryLocation();
+            if (mLocation != null) {
+                internalQueryLocation();
+                listener.onLocationFound(mLocation);
+            } else {
+                mFoundListener = listener;
+            }
         } else {
             new AlertDialog.Builder(mCtx)
                     .setMessage(R.string.prompt_ask_turn_on_location)
@@ -85,36 +94,20 @@ public class LocationHelper {
     }
 
     private void internalQueryLocation() {
-        Location location = mManager.getLastKnownLocation(mProvider);
-        if (location != null && location.getLatitude() != 0 && location.getLongitude() != 0) {
-            mLocation = location;
-            mFoundListener.onLocationFound(location);
-            mFoundListener = null;
-        } else {
-            LocationRequest request = LocationRequest.create() //standard GMS LocationRequest
-                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                    .setExpirationDuration(1000 * SubscriptionTimeout)
-                    .setNumUpdates(1);
-
-            ReactiveLocationProvider locationProvider = new ReactiveLocationProvider(mCtx);
-            mSubscription = locationProvider.getUpdatedLocation(request)
-                    .timeout(SubscriptionTimeout, TimeUnit.SECONDS)
-                    .subscribe(new Action1<Location>() {
-                        @Override
-                        public void call(Location location) {
-                            mSubscription.unsubscribe();
-                            mSubscription = null;
-                            mLocation = location;
-                            mFoundListener.onLocationFound(location);
-                            mFoundListener = null;
-                        }
-                    });
+        try {
+            Location location = LocationServices.FusedLocationApi.getLastLocation(mApiClient);
+            if (location != null && location.getLatitude() != 0 && location.getLongitude() != 0) {
+                mLocation = location;
+                Log.i("lunch", "foudn location to " + location.getLatitude() + ", " + location.getLongitude());
+            }
+        } catch (SecurityException e) {
+            e.printStackTrace();
         }
     }
 
     public void onPause() {
-        if (mSubscription != null && !mSubscription.isUnsubscribed()) {
-            mSubscription.unsubscribe();
+        if (mApiClient.isConnected()) {
+            mApiClient.disconnect();
         }
     }
 
@@ -129,6 +122,9 @@ public class LocationHelper {
                 }
             }
         }
+        if (hasLocationEnabled()) {
+            mApiClient.connect();
+        }
     }
 
     public Location getLastLocation() {
@@ -136,17 +132,10 @@ public class LocationHelper {
             return getCacheLocation();
         }
 
-        if (mLocation != null) {
-            return mLocation;
-        } else {
-            Location location = mManager.getLastKnownLocation(mProvider);
-            if (location != null && location.getLatitude() != 0 && location.getLongitude() != 0) {
-                mLocation = location;
-                return location;
-            } else {
-                return null;
-            }
+        if (mLocation == null) {
+            internalQueryLocation();
         }
+        return mLocation;
     }
 
     public void cacheLocation() {
@@ -176,5 +165,26 @@ public class LocationHelper {
             location.setLongitude(longitudeStr != null ? Float.parseFloat(longitudeStr) : 0);
             return location;
         }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        internalQueryLocation();
+        if (mFoundListener != null) {
+            mFoundListener.onLocationFound(mLocation);
+            mFoundListener = null;
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        if (hasLocationEnabled()) {
+            mApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(TAG, "Geolocation connection failed " + connectionResult.getErrorCode());
     }
 }
